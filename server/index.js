fa01ebe76fe8fbb206e2b39845340beeab471c08
Josiah Maina <josiah5115@gmail.com>
2018-09-13 10:15:57 -0600

    server init

---
+++ server/index.js
@@ -0,0 +1,22 @@
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const axios = require('axios');
+
+const app = express();
+
+const {
+ SERVER_PORT,
+ SESSION_SECRET
+} = process.env;
+
+app.use(session({
+ secret: SESSION_SECRET,
+ resave: false,
+ saveUninitialized: true
+}));
+
+app.listen(SERVER_PORT, () => {
+ console.log(`Server listening on port: ${SERVER_PORT}`)
+});
