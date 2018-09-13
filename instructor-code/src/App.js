f5af26b662fc214b2f6f4a9a853223e7c25f546c
Josiah Maina <josiah5115@gmail.com>
2018-09-13 10:27:09 -0600

    login

--- instructor-code/src/App.js
+++ instructor-code/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <HashRouter>
-        <Switch>
-          <Route path='/' component={} exact />
-        </Switch>
-      </HashRouter>
+        <HashRouter>
+          <Switch>
+            <Route path='/' component={} exact />
+          </Switch>
+        </HashRouter>
       </div>
     );
   }
