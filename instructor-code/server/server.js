896bbd7c4c09a4a025799fa52f464d53c03cd9e7
Josiah Maina <josiah5115@gmail.com>
2018-09-13 11:39:15 -0600

    fixed break/ passing token to get user info

--- instructor-code/server/server.js
+++ instructor-code/server/server.js
@@ -7,7 +7,10 @@ const app = express();
 
 const {
   SERVER_PORT,
-  SESSION_SECRET
+  SESSION_SECRET,
+  REACT_APP_DOMAIN,
+  REACT_APP_CLIENT_ID,
+  CLIENT_SECRET
 } = process.env;
 
 app.use(session({
@@ -15,6 +18,23 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+app.get('/auth/callback', async (req, res) => {
+  let payload = {
+    client_id: REACT_APP_CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code: req.query.code,
+    grant_type: 'authorization_code',
+    redirect_uri: `http://${req.headers.host}/auth/callback`
+  }
+  // post request with code for token
+  let tokenRes = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload);
+  // use token to get user data
+  let userRes = await axios.get(`https://${REACT_APP_DOMAIN}/userinfo?access_token=${tokenRes.data.access_token}`)
+  // put user data on session
+  req.session.user = userRes.data;
+  res.redirect('/')
+})
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port ${SERVER_PORT}`)
--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 
 const {
  SERVER_PORT,
- SESSION_SECRET
+ SESSION_SECRET,
+ REACT_APP_DOMAIN,
+ REACT_APP_CLIENT_ID,
+ CLIENT_SECRET
 } = process.env;
 
 app.use(session({
@@ -16,6 +19,30 @@ app.use(session({
  resave: false,
  saveUninitialized: true
 }));
+
+app.get('/auth/callback', async (req, res) => {
+ // code ====> req.query.code
+ let payload = {
+  client_id: REACT_APP_CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+  code: req.query.code,
+  grant_type: 'authorization_code',
+  redirect_uri: `http://${req.headers.host}/auth/callback`
+ };
+
+ // post request with code for token:
+ let toeknRes = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`, payload)
+
+ // use token to get uesr data:
+ let userRes = await axios.get(`https://${REACT_APP_DOMAIN}/ userinfo?access_token=${toeknRes.data.access_token}`)
+ // console.log(userRes.data)
+
+ // put user data on session:
+ req.session.user = userRes.data;
+ res.redirect('/');
+});
+
+
 
 app.listen(SERVER_PORT, () => {
  console.log(`Server listening on port: ${SERVER_PORT}`)
